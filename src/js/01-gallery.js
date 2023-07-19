3d159311670aa9e769a33f5a678b9268e050c4fc
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-19 15:58:24 +0200

    create_task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
-console.log(galleryItems);
+function createGallery(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join('');
+}
+
+const galleryItemBox = createGallery(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+galleryBox.insertAdjacentHTML('afterbegin', galleryItemBox);
+
+const $lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
