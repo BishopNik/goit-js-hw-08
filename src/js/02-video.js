0002539e446d1a5b7a3a36cd8343836c85a678fa
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-23 15:27:28 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const playerVimeo = new Player(iframe);
+
+const getTimeThrottle = throttle(getTime, 1000);
+
+const timePlayed = localStorage.getItem('videoplayer-current-time');
+if (timePlayed) {
+  playerVimeo.setCurrentTime(timePlayed).catch(errorFn);
+}
+
+playerVimeo.on('timeupdate', getTimeThrottle);
+
+function getTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+function errorFn({ name, message }) {
+  switch (name) {
+    case 'RangeError':
+      console.log('Incorrect time, ', message);
+      break;
+
+    default:
+      console.log('Error:', message);
+      break;
+  }
+}
