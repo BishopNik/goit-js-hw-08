06ccccdb038704e0239a85ca51cb80a70a4b6255
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-23 19:28:30 +0200

    Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,63 @@
+import throttle from 'lodash.throttle';
+const ref = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input[name="email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
+};
 
+const feedbackFormState = {
+  email: '',
+  message: '',
+};
+
+dataFilling();
+
+ref.input.addEventListener('input', throttle(onChangeInput, 500));
+ref.textarea.addEventListener('input', throttle(onChangeInput, 500));
+ref.form.addEventListener('submit', onFormSubmit);
+
+function onChangeInput(evt) {
+  feedbackFormState.email = ref.input.value;
+  feedbackFormState.message = ref.textarea.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  onChangeInput();
+
+  console.log(feedbackFormState);
+
+  ref.form.reset();
+}
+
+function dataFilling() {
+  readDataFormSaved();
+  ref.input.value =
+    feedbackFormState.email !== null && feedbackFormState.email !== ''
+      ? feedbackFormState.email
+      : '';
+  ref.textarea.value =
+    feedbackFormState.message !== null && feedbackFormState.message !== ''
+      ? feedbackFormState.message
+      : '';
+}
+
+function readDataFormSaved() {
+  const formState = localStorage.getItem('feedback-form-state');
+
+  try {
+    feedbackFormStateParse = JSON.parse(formState);
+    feedbackFormState.email = feedbackFormStateParse.email;
+    feedbackFormState.message = feedbackFormStateParse.message;
+  } catch (error) {
+    feedbackFormState.email = '';
+    feedbackFormState.message = '';
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
