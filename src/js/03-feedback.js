f6aa378479e82aa732af6b520554eb56ab0c6804
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-08-14 23:36:53 +0200

    refactoring

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,58 +1,38 @@
 import throttle from 'lodash.throttle';
 const ref = {
   form: document.querySelector('.feedback-form'),
-  input: document.querySelector('input[name="email"]'),
-  textarea: document.querySelector('textarea[name="message"]'),
 };
 
 const LOCALSTR_KEY = 'feedback-form-state';
-const feedbackFormState = {
-  email: '',
-  message: '',
-};
+let feedbackFormState = {};
 
 dataFilling();
 
-ref.input.addEventListener('input', throttle(onChangeInput, 500));
-ref.textarea.addEventListener('input', throttle(onChangeInput, 500));
 ref.form.addEventListener('submit', onFormSubmit);
+ref.form.addEventListener('input', throttle(onChangeInput, 500));
 
 function onChangeInput(evt) {
-  feedbackFormState.email = ref.input.value;
-  feedbackFormState.message = ref.textarea.value;
+  feedbackFormState[evt.target.name] = evt.target.value;
   localStorage.setItem(LOCALSTR_KEY, JSON.stringify(feedbackFormState));
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-
-  onChangeInput();
-
-  console.log(feedbackFormState);
-
   localStorage.removeItem(LOCALSTR_KEY);
-  ref.form.reset();
+  this.reset();
 }
 
 function dataFilling() {
   readDataFormSaved();
-  ref.input.value =
-    feedbackFormState.email !== null && feedbackFormState.email !== ''
-      ? feedbackFormState.email
-      : '';
-  ref.textarea.value =
-    feedbackFormState.message !== null && feedbackFormState.message !== ''
-      ? feedbackFormState.message
-      : '';
+  ref.form.email.value = feedbackFormState.email || '';
+  ref.form.message.value = feedbackFormState.message || '';
 }
 
 function readDataFormSaved() {
   const formState = localStorage.getItem(LOCALSTR_KEY);
 
   try {
-    const feedbackFormStateParse = JSON.parse(formState);
-    feedbackFormState.email = feedbackFormStateParse.email;
-    feedbackFormState.message = feedbackFormStateParse.message;
+    feedbackFormState = JSON.parse(formState);
   } catch (error) {
     feedbackFormState.email = '';
     feedbackFormState.message = '';
